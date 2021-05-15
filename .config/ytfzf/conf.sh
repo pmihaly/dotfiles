fd5505e0c37d7b3c13fef7d6f9afbb15e1c9e9f6
pmihaly <misi@pappmihaly.com>
2021-05-15 12:51:57 +0200

    using devour

--- .config/ytfzf/conf.sh
+++ .config/ytfzf/conf.sh
@@ -1,6 +1,6 @@
-## when using devour, ytfzf hides, but mpv isnt loaded yet
-# video_player="devour mpv"
-# video_player_format="devour mpv --ytdl-format="
-# audio_player="devour mpv --no-video"
+# when using devour, ytfzf hides, but mpv isnt loaded yet
+video_player="devour mpv"
+video_player_format="devour mpv --ytdl-format="
+audio_player="devour mpv --no-video"
 
 enable_hist=0
