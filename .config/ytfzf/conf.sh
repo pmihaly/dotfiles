21f518579dd5c493bf7a3b98c0ae9992745c1c5f
pmihaly <pappmihalyemil@protonmail.com>
2021-05-15 20:09:50 +0200

    Adding URL for st-linkhandler

--- .config/ytfzf/conf.sh
+++ .config/ytfzf/conf.sh
@@ -4,3 +4,12 @@
 # audio_player="devour mpv --no-video"
 
 enable_hist=0
+
+thumbnail_video_info_text () {
+         printf "\n${c_cyan}%s" "$title"
+         printf "\n${c_blue}Channel      ${c_green}%s" "$channel"
+         printf "\n${c_blue}Duration     ${c_yellow}%s" "$duration"
+         printf "\n${c_blue}Views        ${c_magenta}%s" "$views"
+         printf "\n${c_blue}Date         ${c_cyan}%s" "$date"
+         printf "\n${c_blue}URL          ${c_reset}%s" "https://youtube.com/watch?v=$shorturl"
+}
